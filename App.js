989e87d9ab205ca5fe883851d64398cfc956d56f
LexZavala <lex.zavala7@gmail.com>
2022-02-25 01:19:12 -0600

    This is a test commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <><View style={styles.container}>
+      <Text style={styles.text}>Hello World :) My name is Lex and I am ready to learn mobile development!!!</Text>
       <StatusBar style="auto" />
     </View>
+    <View>
+        <ScrollView horizontal>
+        <View style={{ width: 300, height: 300, backgroundColor: 'red' }} />
+          <View style={{ width: 300, height: 300, backgroundColor: 'green' }} />
+          <View style={{ width: 300, height: 300, backgroundColor: 'blue' }} />
+        </ScrollView>
+      </View></>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lavender',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontWeight: "bold",
   },
 });
